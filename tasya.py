22262cc5fbe76048e349995be86956b6a7c4d88b
TasyaBelka (Tatyana Kostyuk) <email.maxx22@gmail.com>
2023-10-23 11:35:11 +0100

    Added functions to tasya.py and tasya_main.py
    In tasya.py added 2 functions to get cards from multiple cards for user and computer.
    In tasya_main.py added functionality for multiple choice and changed getting cards for single card choise

--- tasya.py
+++ tasya.py
@@ -20,7 +20,9 @@ def chosePockemon():
     return random.randint(1, 151)
 
 def showResults(list, param, res1, res2):
-    print(f'Your opponent pockemons:{list}')
+    print('Your opponent pockemon:')
+    showPockemon(list)
+    print()
     print(f'Your {param} is: {res1}')
     print(f'Your opponent {param} is: {res2}')
 
@@ -29,3 +31,18 @@ def showPockemon(dict):
     print(f"Id: {dict['id']}")
     print(f"Weight: {dict['weight']}")
     print(f"Height: {dict['height']}")
+
+# getting pockemon from the computer list with max param value
+def computerPockemon(list, param):
+    paramValueList = []
+    for i in range(len(list)):
+        paramValueList.append(list[i][param])
+    maxValue = max(paramValueList)
+
+    for i in range(len(list)):
+        if list[i][param] == maxValue:
+            return list[i]
+
+# getting chosen pockemon from the user's pockemon list
+def findUserPockemon(list, number):
+    return list[number]
--- tasya_main.py
+++ tasya_main.py
@@ -1,5 +1,6 @@
-# import functions
+# import functions from tasya.py file
 from tasya import getPockemon, chosePockemon, showResults, showPockemon
+from tasya import findUserPockemon, computerPockemon
 
 # start the game
 # ask user to play
@@ -8,54 +9,100 @@ userScore = 0
 computerScore = 0
 greetingMsg = "Welcome to the Pockemon game! Let's play?(y/n): "
 
+# start game 
 while answer == 'y':
     userAnswer = input(greetingMsg)
     answer = userAnswer
     greetingMsg = 'Do you want to play again?(y/n): '
 
     if userAnswer == 'y':
-        
-        userPockemonList = []
-        computerPockemonList = []
-
-        cardsNumber = int(input('How many cards do you want?: '))
+        # ask user how many cards he/she wants to play with
+        cardsNumber = int(input('How many cards do you want to play with?: '))
 
         if cardsNumber > 1:
+            # creating empty lists of pockemons for user and computer
+            userPockemonList = []
+            computerPockemonList = []
+
             # show user's pockemons
             print('\nYour pockemons:')
             
             for i in range(cardsNumber):
-                userPockemonList.append(getPockemon(chosePockemon()))
+                # fill the user pockemon list and show it to user
+                userPockemonList.append(getPockemon(chosePockemon())) 
 
                 print(f'\nPockemon number {i + 1}:\n')
                 showPockemon(userPockemonList[i])
 
-                computerPockemonList.append(getPockemon(chosePockemon()))
+                # fill the computer pockemon list
+                computerPockemonList.append(getPockemon(chosePockemon())) 
 
-            print('\n')
-            userCardChoise = input('Chose the pockemon number: ')    
+            print()
             userParamChoise = input('Choose parameter:(id, height, weight) ')
+            userCardChoise = int(input('Chose the pockemon number: '))   
+            
+            # getting 1 chosen card from user and with max value computer card
+            computerCard = computerPockemon(computerPockemonList, userParamChoise)
+            userCard = findUserPockemon(userPockemonList, userCardChoise - 1)
+            print() 
 
-        else:
-            print(f'Your pockemon: {showPockemon(userPockemonList[0])}')
-            userParamChoise = input('Choose parameter:(id, height, weight) ')
+            print(f'You choose: {userCard["name"].upper()}!')
+            showPockemon(userCard)
+            print()
+            
+            # show computer pockemon cards
+            print('Your opponent pockemon cards:')
+            for i in range(len(computerPockemonList)):
+                showPockemon(computerPockemonList[i])
+                print()
+            
+            # getting the results of chosen parameter
+            userResult = userCard[userParamChoise]
+            compResult = computerCard[userParamChoise]
 
-            # compare parameters
-            userResult = userPockemonList[0][userParamChoise]
-            compResult = computerPockemonList[0][userParamChoise]
-
+            # compare results and getting score
             if userResult > compResult:
                 userScore += 1
-                showResults(computerPockemonList, userParamChoise, userResult, compResult)
+                showResults(computerCard, userParamChoise, userResult, compResult)
                 print(f'You won! The score is: {userScore}/{computerScore}')
 
             elif userResult < compResult:
                 computerScore += 1
-                showResults(computerPockemonList, userParamChoise, userResult, compResult)
+                showResults(computerCard, userParamChoise, userResult, compResult)
+                print(f'You lose. The score is: {userScore}/{computerScore}')
+            else:
+                showResults(computerCard, userParamChoise, userResult, compResult)
+                print(f'Equal! The score is: {userScore}/{computerScore}')
+
+        else:
+            # getting the pockemons for user and computer
+            userPockemon = getPockemon(chosePockemon())
+            compPockemon = getPockemon(chosePockemon())
+            print()
+
+            # show user's pockemon card
+            print(f'Your pockemon: ')
+            showPockemon(userPockemon)
+            userParamChoise = input('Choose parameter:(id, height, weight) ')
+            print()
+
+            # getting values of chosen parameter for user and computer 
+            userResult = userPockemon[userParamChoise]
+            compResult = compPockemon[userParamChoise]
+
+            # compare results
+            if userResult > compResult:
+                userScore += 1
+                showResults(compPockemon, userParamChoise, userResult, compResult)
+                print(f'You won! The score is: {userScore}/{computerScore}')
+
+            elif userResult < compResult:
+                computerScore += 1
+                showResults(compPockemon, userParamChoise, userResult, compResult)
                 print(f'You lose. The score is: {userScore}/{computerScore}')
             else:
                 print(f'Equal! The score is: {userScore}/{computerScore}')
 
     else:
+        # show the final score after ending the game
         print(f'The final score is: {userScore}/{computerScore}\nGoodbye!')
-
